e3cf23dbbdab1eaf37abb2ec86a7bfbace2a0f63
RashmiBalaji <rashmimenonv@gmail.com>
2020-12-04 11:27:03 +0100

    Feat: form validation re-factored with state

--- src/components/BillingPage/billingpage.js
+++ src/components/BillingPage/billingpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../../App.css";
 import { makeStyles } from "@material-ui/core/styles";
@@ -22,9 +22,30 @@ const useStyles = makeStyles((theme) => ({
 
 // eslint-disable-next-line import/no-anonymous-default-export
 function BillingPage(props) {
+  const [card,setCard] = useState('')
+  const [password,setPassword]=useState('')
+  const [validation, setValidation] =useState(false)
+
   const classes = useStyles();
   const data = useSelector((store) => store.cart);
 
+  function handleCardChange (e) {
+    setCard(parseInt(e.target.value));
+  }
+  function handlePasswordChange (e) {
+    setPassword(parseInt(e.target.value));
+  }
+
+  function handleSubmit (e) {
+    e.preventDefault();
+    if (!card || !password) {
+        window.alert("Enter all required details")
+    } else {
+      props.placeOrder(data)
+        console.log("validation done on ORDERS form")
+        setValidation(true)
+    }
+  }
   console.log(data, "cart initial from billing page");
 
   return (
@@ -32,34 +53,45 @@ function BillingPage(props) {
     <div style={{paddingTop:"10vh"}}>
       <h2 className="checkout">Billing Particulars</h2>
       <div className="form">
-        <form className={classes.root}>
+        <form className={classes.root} onSubmit={handleSubmit}>
           <TextField
             required
             id="outlined-required"
             label="Enter your card number here"
             variant="outlined"
+            value={card}
+            onChange={handleCardChange}
           />
           <TextField
             required
             id="outlined-required"
             label="Enter CVV here"
             variant="outlined"
+            value={password}
+            onChange={handlePasswordChange}
           />
           <div class="buttons">
-          <Link to={{
+          {/* <Link to={{
     pathname:"/orders"
-}} style={{textDecoration:'none'}}>
-            <Button
+}} style={{textDecoration:'none'}}> */}
+            <button
               style={{ width: "100px", margin: "5px 10px 10px 10px" }}
               variant="contained"
+              TYPE="submit"
               size="large"
               color="secondary"
               href="/orders"
-              onClick={() => props.placeOrder(data)}
+              // onClick={() => props.placeOrder(data)}
             >
               CONFIRM
-            </Button>
-</Link>
+            </button>
+{/* </Link> */}
+{validation && (
+                 <Redirect to={{
+                    pathname:"/orders"
+                }} style={{textDecoration:'none'}}>
+                    </Redirect>
+            )}
             <Button
               style={{ width: "100px", margin: "5px 10px 10px 10px" }}
               variant="contained"
--- src/components/OrderPage/orderpage.js
+++ src/components/OrderPage/orderpage.js
@@ -5,6 +5,7 @@ import { getState , useDispatch, useSelector } from 'react-redux';
 import CartPage from '../CartPage/cartpage.js'
 import store from '../../store/configureStore.js'
 import Button from "@material-ui/core/Button";
+import './orderpage.css'
 import { connect } from 'react-redux'
 import { addProductToCart } from "../../actions/cartactions.js"
 
@@ -19,15 +20,20 @@ console.log(cartdata,"cart data using use selector from orders page")
 const data = useSelector((store) => store.orders)
 console.log(data,"order data using use selector from orders page")
 
+const totalPrice = data.reduce((a,b)=>a+(b.price*b.quantity),0)
 
 
   const inventoryList = data.map((inventory) => (
     <>
-      <li className="item-tile">
-        <img src={inventory.image} alt="pics" width="200" height="150" />
-        <h4>{inventory.title}</h4>
-        <h3>{`${inventory.price} Euros`}</h3>
-       
+      <li className="individual-order">
+          <div className="left-cart">
+        <img src={inventory.image} alt="pics" width="100" height="100" />
+        </div>
+        <div className="right-cart">
+        <p>{inventory.title}</p>
+        <p>Quantity: {inventory.quantity}</p>
+        <p>{`${inventory.price} Euros`}</p>
+        </div>
       </li>
     </>
   ));
@@ -41,7 +47,12 @@ console.log(data,"order data using use selector from orders page")
       <div style={{paddingTop:"10vh", marginLeft:"10vh"}}>
         <h4>Your order for the following items are placed:</h4>
         {inventoryList.length>0 && (
-            <ul className="inventory-list">{inventoryList}</ul>
+            <>
+            <div className="container-cart">
+            <ul className="order-list">{inventoryList}</ul>
+            </div>
+            <h3>Your total order value: {totalPrice} Euros</h3>
+            </>
         )
         }
              {inventoryList.length===0 && (
